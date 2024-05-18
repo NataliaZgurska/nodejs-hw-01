63ad9c7ae97ad85e3e59fba0bf7a3a44a335b579
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-05-18 15:38:36 +0300

    update create contact

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 import fs from 'node:fs/promises';
-// import { createFakeContact } from './utils/createFakeContact';
-// import { PATH_DB } from '../constants/contacts.js';
 import { faker } from '@faker-js/faker';
-import { createFakeContact } from './utils/createFakeContact';
 
-// export const createFakeContact = () => ({
-//   name: faker.person.fullName(),
-//   phone: faker.phone.number(),
-//   email: faker.internet.email(),
-//   job: faker.person.jobTitle(),
-// });
+const createFakeContact = () => ({
+  name: faker.person.fullName(),
+  phone: faker.phone.number(),
+  email: faker.internet.email(),
+  job: faker.person.jobTitle(),
+});
 
-const generateContacts = async () => {
-  const newContact = createFakeContact();
-  const newContactString = JSON.stringify(newContact);
+const generateContacts = async (number) => {
+  let contactsArray = [];
+
+  for (let i = 0; i < number; i++) {
+    const newContact = createFakeContact();
+    contactsArray.push(newContact);
+  }
+
   try {
-    await fs.writeFile('src/text.txt', newContactString);
-    console.log(newContactString);
+    const content = await fs.readFile('src/text.txt');
+    // if (!content) {      const contactsArrayNew = [...contactsArray]; }
+    const contentParse = JSON.parse(content);
+    const contactsArrayNew = [...contentParse, ...contactsArray];
+
+    console.log(contactsArrayNew);
+    const contactsArrayNewString = JSON.stringify(contactsArrayNew);
+    await fs.writeFile('src/text.txt', contactsArrayNewString);
   } catch (err) {
     console.error('Error', err);
   }
 };
 
-await generateContacts();
+await generateContacts(2);
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -3,19 +3,3 @@
 // const generateContacts = async (number) => {};
 
 // await generateContacts(5);
-
-// export function createRandomUser() {
-//   return {
-//     userId: faker.string.uuid(),
-//     username: faker.internet.userName(),
-//     email: faker.internet.email(),
-//     avatar: faker.image.avatar(),
-//     password: faker.internet.password(),
-//     birthdate: faker.date.birthdate(),
-//     registeredAt: faker.date.past(),
-//   };
-// }
-
-// export const users = faker.helpers.multiple(createRandomUser, {
-//   count: 5,
-// });
